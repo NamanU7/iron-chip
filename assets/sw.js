7b9e8c3d378b849221c9efaedf79d04e0ce685e8
Naman Uttamchandani <naman.uttam7@gmail.com>
2023-05-31 20:01:16 -0400

    Change a few details to fix build failures

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = "egui-template-pwa";
 var filesToCache = [
   "./",
   "./index.html",
-  "./eframe_template.js",
-  "./eframe_template_bg.wasm",
+  "./iron_chip.js",
+  "./iron_chip_bg.wasm",
 ];
 
 /* Start the service worker and cache all of the app's content */
